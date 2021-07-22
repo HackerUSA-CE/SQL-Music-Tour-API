fbe76341c23e574c5417ad696cdf6a09c6077d7b
Jerrica Bobadilla <jerrica@Jerricas-iMac.home>
2021-07-22 10:46:51 -0700

    add part2-8.5.2

---
+++ models/band.js
@@ -0,0 +1,38 @@
+// DEPENDENCIES
+const { Sequelize, DataTypes,  Model } = require('sequelize')
+const sequelize = new Sequelize(process.env.PG_URI)
+
+// MODEL
+class Band extends Model{}
+
+Band.init({
+    band_id: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true,  
+        allowNull: false 
+    },
+    name: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    },
+    genre: { 
+        type: DataTypes.TEXT, 
+        allowNull: false 
+    },
+    available_start_time: { 
+        type: DataTypes.DATE, 
+        allowNull: false 
+    },
+    end_time: { 
+        type: DataTypes.DATE, 
+        allowNull: false 
+    },
+}, {
+    sequelize,                           
+    modelName: 'Band',
+    tableName: 'band',
+    timestamps: false
+}) 
+
+// EXPORT
+module.exports = Band
