c29922f9d21c6b4ff2550b47127ea96231797fcb
Jerrica Bobadilla <jerrica@Jerricas-iMac-5.home>
2021-07-28 13:56:42 -0700

    add part 3

--- models/band.js
+++ models/band.js
@@ -1,38 +1,44 @@
-// DEPENDENCIES
-const { Sequelize, DataTypes,  Model } = require('sequelize')
-const sequelize = new Sequelize(process.env.PG_URI)
-
-// MODEL
-class Band extends Model{}
-
-Band.init({
-    band_id: { 
-        type: DataTypes.INTEGER, 
-        primaryKey: true,  
-        allowNull: false 
+'use strict'
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Band extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Band.init({
+    band_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
-    name: { 
-        type: DataTypes.STRING, 
-        allowNull: false 
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    genre: { 
-        type: DataTypes.TEXT, 
-        allowNull: false 
+    genre: {
+        type: DataTypes.TEXT,
+        allowNull: false
     },
-    available_start_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
+    available_start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
     },
-    end_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
-    },
-}, {
-    sequelize,                           
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+  }, {
+    sequelize,
     modelName: 'Band',
     tableName: 'band',
     timestamps: false
-}) 
-
-// EXPORT
-module.exports = Band
+  })
+  return Band
+}
--- server.js
+++ server.js
@@ -8,16 +8,6 @@ require('dotenv').config()
 express.json()
 express.urlencoded({ extended: false })
 
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize(process.env.PG_URI)
-
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
-
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
