662723b557b3f1f94af4a91543a25afe34877c0e
Jerrica Bobadilla <jerrica@Jerricas-iMac.home>
2021-08-11 16:32:26 -0700

    .

---
+++ migrations/20210811232709-add-recommendation-column-to-bands.js
@@ -0,0 +1,14 @@
+'use strict'
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('bands', 'recommendation', {
+      type: DataTypes.STRING
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('bands', 'recommendation')
+  }
+};
