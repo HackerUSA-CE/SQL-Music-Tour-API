bc46214323d81c3e666af6a30654a84164510aeb
Jerrica Bobadilla <jerrica@Jerricas-iMac.home>
2021-08-12 15:00:26 -0700

    update index route with advance querying

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -2,11 +2,17 @@
 const bands = require('express').Router()
 const db = require('../models')
 const { Band } = db 
+const { Op } = require('sequelize')
 
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC' ] ],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+            }
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
