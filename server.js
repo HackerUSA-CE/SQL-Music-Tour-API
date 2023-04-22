265ae8a15d27ccbd0761e34bb7d004779352c379
bmartin-dealersocket <bmartin@dealersocket.com>
2023-04-22 08:19:15 -0500

    adjusted controller setup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const bandsController = require('./controllers/bands_controller')
+const eventsController = require('./controllers/events_controller')
+const stagesController = require('./controllers/stages_controller')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
@@ -14,11 +17,8 @@ app.get('/', (req, res) => {
     })
 })
 
-const bandsController = require('./controllers/bands_controller')
 app.use('/bands', bandsController)
-const eventsController = require('./controllers/events_controller')
 app.use('/events', eventsController)
-const stagesController = require('./controllers/stages_controller')
 app.use('/stages', stagesController)
 
 // LISTEN
