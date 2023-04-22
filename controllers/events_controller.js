f30771968e63a58bfa13aac0e3b7135d15bdf04a
bmartin-dealersocket <bmartin@dealersocket.com>
2023-04-22 11:51:57 -0500

    Changes to stages and events controllers

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -1,76 +1,117 @@
 const events = require('express').Router();
 const db = require('../models');
-const { Event } = db;
+const { Event, Stage, StageEvent, SetTime, MeetGreet, Band } = db;
 const { Op } = require('sequelize');
 
 //! CREATE
 events.post('/', async (req, res) => {
-    try {
-        const newEvent = await Event.create(req.body)
-        res.status(200).json({
-            message: 'Created a new event',
-            data: newEvent
-        })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+	try {
+		const newEvent = await Event.create(req.body);
+		res.status(200).json({
+			message: 'Created a new event',
+			data: newEvent,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 //! READ
 events.get('/', async (req, res) => {
-    try {
-        const foundEvents = await Event.findAll({
-            order: [ [ 'date', 'ASC' ] ],
-            where: {
-                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
-            }
-        })
-        res.status(200).json(foundEvents)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
-events.get('/:id', async (req, res) => {
-    try {
-        const foundEvent = await Event.findOne({
-            where: { event_id: req.params.id }
-        })
-        res.status(200).json(foundEvent)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+	try {
+		const foundEvents = await Event.findAll({
+			order: [['date', 'ASC']],
+			where: {
+				name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` },
+			},
+		});
+		res.status(200).json(foundEvents);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+events.get('/:name', async (req, res) => {
+	try {
+		const foundEvent = await Event.findOne({
+			where: { name: req.params.name },
+			attributes: { exclude: ['event_id'] },
+			include: [
+				{
+					model: MeetGreet,
+					as: 'meet_greets',
+					attributes: ['meet_start_time', 'meet_end_time'],
+					include: {
+						model: Band,
+						as: 'band',
+						attributes: ['name'],
+					},
+				},
+				{
+					model: SetTime,
+					as: 'set_times',
+					attributes: ['start_time', 'end_time'],
+					include: [
+						{
+							model: Band,
+							as: 'band',
+							attributes: ['name'],
+						},
+						{
+							model: Stage,
+							as: 'stage',
+							attributes: ['stage_name'],
+						},
+					],
+				},
+				{
+					model: Stage,
+					as: 'stages',
+					attributes: { exclude: ['stage_id'] },
+					through: { attributes: [] },
+				},
+			],
+            order: [
+                [{model: MeetGreet, as:'meet_greets'}, 'meet_start_time', 'ASC'],
+                [{model: SetTime, as:'set_times'}, 'start_time', 'ASC'],
+                [{model: Stage, as: 'stages'}, 'stage_name', 'ASC']
+            ]
+		});
+		res.status(200).json(foundEvent);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 //! UPDATE
 events.put('/:id', async (req, res) => {
-    try {
-        const updatedEvents = await Event.update(req.body, {
-            where: {
-                event_id: req.params.id
-            }
-        })
-        res.status(200).json({
-            message: `Successfully updated ${updatedEvents} event(s)`
-        })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+	try {
+		const updatedEvents = await Event.update(req.body, {
+			where: {
+				event_id: req.params.id,
+			},
+		});
+		res.status(200).json({
+			message: `Successfully updated ${updatedEvents} event(s)`,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 //! DELETE
 events.delete('/:id', async (req, res) => {
-    try {
-        const deletedEvents = await Event.destroy({
-            where: {
-                event_id: req.params.id
-            }
-        })
-        res.status(200).json({
-            message: `Successfully deleted ${deletedEvents} event(s)`
-        })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+	try {
+		const deletedEvents = await Event.destroy({
+			where: {
+				event_id: req.params.id,
+			},
+		});
+		res.status(200).json({
+			message: `Successfully deleted ${deletedEvents} event(s)`,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 module.exports = events;
--- controllers/stages_controller.js
+++ controllers/stages_controller.js
@@ -1,75 +1,83 @@
-const stages = require('express').Router()
-const db = require('../models')
-const { Stage } = db 
-const { Op } = require('sequelize')
+const stages = require('express').Router();
+const db = require('../models');
+const { Stage, Event, StageEvent } = db;
+const { Op } = require('sequelize');
 
 //! CREATE
 stages.post('/', async (req, res) => {
-    try {
-        const newStage = await Stage.create(req.body)
-        res.status(200).json({
-            message: 'Created a new stage',
-            data: newStage
-        })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+	try {
+		const newStage = await Stage.create(req.body);
+		res.status(200).json({
+			message: 'Created a new stage',
+			data: newStage,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 //! READ
 stages.get('/', async (req, res) => {
-    try {
-        const foundStages = await Stage.findAll({
-            where: {
-                stage_name: { [Op.like]: `%${req.query.stage_name ? req.query.stage_name : ''}%` }
-            }
-        })
-        res.status(200).json(foundStages)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
-stages.get('/:id', async (req, res) => {
-    try {
-        const foundStage = await Stage.findOne({
-            where: { stage_id: req.params.id }
-        })
-        res.status(200).json(foundStage)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+	try {
+		const foundStages = await Stage.findAll({
+			where: {
+				stage_name: { [Op.like]: `%${req.query.stage_name ? req.query.stage_name : ''}%` },
+			},
+		});
+		res.status(200).json(foundStages);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+stages.get('/:name', async (req, res) => {
+	try {
+		const foundStage = await Stage.findOne({
+			where: { stage_name: req.params.name },
+			include: {
+				model: Event,
+				as: 'events',
+				attributes: ['name', 'date'],
+				through: { attributes: [] },
+			},
+			attributes: { exclude: ['stage_id'] },
+			order: [[{ model: Event, as: 'events' }, 'date', 'ASC']],
+		});
+		res.status(200).json(foundStage);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 //! UPDATE
 stages.put('/:id', async (req, res) => {
-    try {
-        const updatedStages = await Stage.update(req.body, {
-            where: {
-                stage_id: req.params.id
-            }
-        })
-        res.status(200).json({
-            message: `Successfully updated ${updatedStages} stage(s)`
-        })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+	try {
+		const updatedStages = await Stage.update(req.body, {
+			where: {
+				stage_id: req.params.id,
+			},
+		});
+		res.status(200).json({
+			message: `Successfully updated ${updatedStages} stage(s)`,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
 //! DELETE
 stages.delete('/:id', async (req, res) => {
-    try {
-        const deletedStages = await Stage.destroy({
-            where: {
-                stage_id: req.params.id
-            }
-        })
-        res.status(200).json({
-            message: `Successfully deleted ${deletedStages} stage(s)`
-        })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+	try {
+		const deletedStages = await Stage.destroy({
+			where: {
+				stage_id: req.params.id,
+			},
+		});
+		res.status(200).json({
+			message: `Successfully deleted ${deletedStages} stage(s)`,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
-module.exports = stages
+module.exports = stages;
