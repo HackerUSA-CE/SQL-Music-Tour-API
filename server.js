2dadce6963b62847a38df329fbe2485ef98ebe75
Anthony Jesmok <anthony@jesmok.xyz>
2023-09-06 18:46:43 -0500

    Utilize Sequelize

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate()
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+} catch (err) {
+    console.log(`Unable to connect to PG: ${err}`)
+}
 
 // ROOT
 app.get('/', (req, res) => {
