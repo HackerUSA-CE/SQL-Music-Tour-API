a57eee5b9d9c41f93bbdfe972414a24876d66674
Chris <117781333+chrism-la@users.noreply.github.com>
2024-02-01 05:25:03 -0800

    1. created new instance of sequelize 2. created Band class by extending sequelize Model class 3. exported model

--- models/band.js
+++ models/band.js
@@ -1,1 +1,6 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize(process.env.PG_URI);
+
+export default class Band extends Model {}
+
+Band.init({}, { sequelize, modelName: 'Band', tableName: 'band', timestamps: false });
