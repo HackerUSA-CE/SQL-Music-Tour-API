e6b90597c25797a32f6f7a7438a37ff5caec870a
Chris <117781333+chrism-la@users.noreply.github.com>
2024-02-01 05:30:43 -0800

    created object to specify columns and datatypes

--- models/band.js
+++ models/band.js
@@ -3,4 +3,29 @@ const sequelize = new Sequelize(process.env.PG_URI);
 
 export default class Band extends Model {}
 
-Band.init({}, { sequelize, modelName: 'Band', tableName: 'band', timestamps: false });
+Band.init(
+    {
+        band_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        genre: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        available_start_time: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        end_time: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    },
+    { sequelize, modelName: 'Band', tableName: 'band', timestamps: false }
+);
