fd9c6305a1e3be2c66d60f6d2391123f37a011e5
Chris <117781333+chrism-la@users.noreply.github.com>
2024-02-01 05:49:08 -0800

    fixed migration file

--- migrations/20240201134252-create-band.js
+++ migrations/20240201134252-create-band.js
@@ -1,40 +1,33 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Bands', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      band_id: {
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      genre: {
-        type: Sequelize.TEXT
-      },
-      available_start_time: {
-        type: Sequelize.DATE
-      },
-      end_time: {
-        type: Sequelize.DATE
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bands');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('bands', {
+            band_id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            genre: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            available_start_time: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            end_time: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('bands');
+    },
 };
