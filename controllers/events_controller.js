f5deeda755396af871a55509dc7385947aaa2cc7
Chris <117781333+chrism-la@users.noreply.github.com>
2024-02-02 03:49:09 -0800

    created events controller

---
+++ controllers/events_controller.js
@@ -0,0 +1,73 @@
+const events = require('express').Router();
+const db = require('../models');
+const { Event } = db;
+const { Op } = require('sequelize');
+
+events.get('/', async (req, res) => {
+    try {
+        const foundEvents = await Event.findAll({
+            order: [['date', 'ASC']],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` },
+            },
+        });
+        res.status(200).json(foundEvents);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+events.get('/:id', async (req, res) => {
+    try {
+        const foundEvent = await Event.findOne({
+            where: { event_id: req.params.id },
+        });
+        res.status(200).json(foundEvent);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+events.post('/', async (req, res) => {
+    try {
+        const newEvent = await Event.create(req.body);
+        res.status(200).json({
+            message: 'Successfully inserted a new event',
+            data: newEvent,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+events.put('/:id', async (req, res) => {
+    try {
+        const updatedEvents = await Event.update(req.body, {
+            where: {
+                event_id: req.params.id,
+            },
+        });
+        res.status(200).json({
+            message: `Successfully updated ${updatedEvents} event(s)`,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+events.delete('/:id', async (req, res) => {
+    try {
+        const deletedEvents = await Event.destroy({
+            where: {
+                event_id: req.params.id,
+            },
+        });
+        res.status(200).json({
+            message: `Successfully deleted ${deletedEvents} event(s)`,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = events;
