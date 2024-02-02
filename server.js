5af62ab053e4e13eac0ef5d82a268646179b592b
Chris <117781333+chrism-la@users.noreply.github.com>
2024-02-02 03:54:30 -0800

    reconfigured entry file

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-// DEPENDENCIES
 const express = require('express');
 const app = express();
+const { Sequelize } = require('sequelize');
 
-// CONFIGURATION / MIDDLEWARE
 require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API',
     });
 });
 
-// LISTEN
+const bandsController = require('./controllers/bands_controller');
+app.use('/bands', bandsController);
+
+const eventsController = require('./controllers/events_controller');
+app.use('/events', eventsController);
+
+const stagesController = require('./controllers/stages_controller');
+app.use('/stages', stagesController);
+
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
 });
